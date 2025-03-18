fe8cdc6ed7e7a6aca032314bd77ded8ed7aa4b68
Gabriela de Melo Freitas <45526185800@>
2025-03-18 10:39:56 -0300

    Atividade API Carros

---
+++ PWBE_codigos/api/api/app_api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Carro(models.Model):
+    nome = models.CharField(max_length=255)
+    marca = models.CharField(max_length=255)
+    qntRodas = models.PositiveBigIntegerField()
+    ano = models.PositiveBigIntegerField()
+    cor = models.CharField(max_length=255)
+    escolhas_combustivel = (
+        ('GASOLINA', 'Gasolina'),
+        ('ETANOL', 'Etanol'),
+        ('GNV', 'GNV'), 
+        ('ELETRICO', 'Eletrico'),
+        ('ALCOOL', 'Alcool'), 
+        ('DIESEL', 'Diesel'), 
+        ('FB', 'FeedBack'),
+        )
+    combustivel = models.CharField(max_length=9, choices=escolhas_combustivel)
+
+    def __str__(self):
+        return self.nome
